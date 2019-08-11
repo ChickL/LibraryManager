f6d80608569d28d1185ba989cfb4ccf4b5ce14da
ChickL <40787727+ChickL@users.noreply.github.com>
2019-08-11 16:40:50 +0800

    Add files via upload

---
+++ qt_include.h
@@ -0,0 +1,23 @@
+#ifndef QT_INCLUDE_H
+#define QT_INCLUDE_H
+
+#include <QObject>
+#include <QString>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QTabWidget>
+#include <QPushButton>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QCheckBox>
+#include <QPalette>
+#include <QPixmap>
+#include <QStringList>
+#include <QComboBox>
+
+#endif // QT_INCLUDE_H
